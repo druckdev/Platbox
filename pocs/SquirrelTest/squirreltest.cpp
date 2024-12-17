9a16705c0c80b9972f68d1acc89d0f4237600d72
Julian Prein <julian@druckdev.xyz>
2024-12-17 11:10:55 +0000

    Fix squirrel test POC

--- pocs/SquirrelTest/squirreltest.cpp
+++ pocs/SquirrelTest/squirreltest.cpp
@@ -21,29 +21,28 @@
 // Find through `lspci -vvv -s $BUS:$DEV.$FUNC`
 #define SQUIRREL_BAR 0xD0500000
 
-void enable_squirrel()
-{
-	pci_enable_memory_space(SQUIRREL_BUS, SQUIRREL_DEV, SQUIRREL_FNC);
-	pci_enable_bus_master(SQUIRREL_BUS, SQUIRREL_DEV, SQUIRREL_FNC);
-}
-
-void clear_bar_buffer()
-{
-	// Clear BAR Buffer
-	void* dma_buf_page = map_physical_memory(SQUIRREL_BAR, PAGE_SIZE);
-	memset(dma_buf_page, 0x00, PAGE_SIZE);
-	print_memory(SQUIRREL_BAR, (char *) dma_buf_page, 0x100);
-	// getchar();
-	unmap_physical_memory(dma_buf_page, PAGE_SIZE);
-}
-
 int main(int argc, char** argv)
 {
-	enable_squirrel();
-	clear_bar_buffer();
+	enable_debug_mode();
 
-	UINT32 *squirrel = (UINT32 *) SQUIRREL_BAR;
+	open_platbox_device();
+
+	// Enable Squirrel
+	pci_enable_memory_space(SQUIRREL_BUS, SQUIRREL_DEV, SQUIRREL_FNC);
+	pci_enable_bus_master(SQUIRREL_BUS, SQUIRREL_DEV, SQUIRREL_FNC);
+
+	// Clear BAR buffer (TODO: why?)
+	void* dma_buf_page = map_physical_memory(SQUIRREL_BAR, PAGE_SIZE);
+	// memset(dma_buf_page, 0x00, PAGE_SIZE);
+	// print_memory(SQUIRREL_BAR, (char *) dma_buf_page, 0x100);
+	// getchar();
+
+	// UINT32 *squirrel = (UINT32 *) SQUIRREL_BAR;
+	UINT32 *squirrel = (UINT32*)dma_buf_page;
 	squirrel[32] = 0x42424242;
+
+	unmap_physical_memory(dma_buf_page, PAGE_SIZE);
+	close_platbox_device();
 
 	return 0;
 }
